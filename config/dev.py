01aac607df40401669185ba594bbeca4ff6e0ed9
Toygrammer <gdh0608@naver.com>
2018-07-06 09:03:38 +0900

    DB URL 수정

--- config/dev.py
+++ config/dev.py
@@ -5,7 +5,7 @@ class DevConfig(Config):
     DEBUG = True
     SQLALCHEMY_ECHO = True
 
-    SQLALCHEMY_DATABASE_URI = 'postgres://localhost:5432/poem?' \
+    SQLALCHEMY_DATABASE_URI = 'postgres://localhost:5432/poem?' + \
                               'user=postgres&password=ehdgus0608'
 
 
