1b2488e8f740cf143af305d68c237285a68b1e38
Toygrammer <gdh0608@naver.com>
2018-07-11 23:38:20 +0900

    Dynamic Import 수정

--- run_server.py
+++ run_server.py
@@ -1,8 +1,9 @@
-if __name__ == '__main__':
-    from app import create_app
-    from config import DevConfig
+from app import create_app
+from config import DevConfig
 
+if __name__ == '__main__':
     app = create_app(DevConfig)
     app.run(port=app.config['PORT'],
             debug=app.config['DEBUG'],
             threaded=True)
+
