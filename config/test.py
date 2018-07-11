5d6b5683718e7e803139995511853a45f75ad444
Toygrammer <gdh0608@naver.com>
2018-07-11 23:38:48 +0900

    TestConfig 수정
    SQLALCHEMY_ECHO 값을 True로 설정

--- config/test.py
+++ config/test.py
@@ -2,6 +2,7 @@ from config import Config
 
 
 class TestConfig(Config):
+    SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = 'postgres://localhost:5432/poem_test?' + \
                               'user=postgres&password=ehdgus0608'
 
